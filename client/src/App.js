d3b4b1cfdbcb9f4f2338034b4f492e728b1c2ea8
Robby Compton <rcbetarick@gmail.com>
2019-08-23 21:00:08 -0400

    Core Features (~2.5-3hrs)
     - Expanded APP to make api calls on render and load the data.
     - Added data combination and pagination to the api.
     - Formatted phone numbers. Want to dig more through antd to see if you
     can pass transformation functions to control display of data rather
     than having to mutate the original data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,132 @@
 import React from 'react'
 import { Card, Table } from 'antd'
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
 import './App.css'
+//import { threadId } from 'worker_threads';
 
-function App() {
-  return (
-    <main className="App">
-      <section className="App-header">
-        <h1>Numbers</h1>
-        <div className="auto" />
-        <span>AVOXI Take-Home</span>
-      </section>
-      <section className="App-card">
-        <Card
-          className="App-card__inner"
-          title="Available Numbers"
-          bordered={true}
-        >
-          <Table />
-        </Card>
-      </section>
-    </main>
-  )
+const columns = [
+  {
+    title: 'Phone Number',
+    dataIndex: 'number',
+    key: 'number'
+  },
+  {
+    title: 'Country',
+    dataIndex: 'country',
+    key: 'country'
+  },
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    key: 'type'
+  },
+  {
+    title: 'Answering Mode',
+    dataIndex: 'answering_mode',
+    key: 'manswering_ode'
+  },
+  {
+    title: 'Assigned To',
+    dataIndex: 'user',
+    key: 'user'
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numbers: [],
+      pagination: {},
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  onTableChange = (pagination, filters, sorter) => {
+    // Copy the state pagination to a new one.
+    const pager = { ...this.state.pagination};
+    // Get the current page from the given pagination from the table change event.
+    pager.current = pagination.current;
+    // Update the state with the pagination.
+    this.setState({
+      pagination: pager
+    });
+    this.fetchData({
+      size: pagination.pageSize,
+      page: pagination.current
+    });
+  }
+
+  async fetchData(params = {}) {
+    this.setState({ loading: true });
+    try {
+      let uri = '/api/numbers?page=';
+      uri += encodeURIComponent(params.page ? params.page : 1);
+      uri += "&size="
+      uri += encodeURIComponent(params.size ? params.size : 10);
+
+      let res = await fetch(uri).then(res => res.json());
+
+      if (res.error) {
+        console.log("Error fetching numbers: ", res.message);
+      } else {
+        const numbers = res.message;
+
+        for (let number of numbers) {
+          if (number.first_name) number.user = number.first_name + " " + number.last_name;
+          const phoneNumber = parsePhoneNumberFromString("+" + number.number.toString());
+          if (phoneNumber) {
+            number.number = phoneNumber.formatInternational();
+            number.country = phoneNumber.country;
+          }
+        }
+
+        const pagination = {...this.state.pagination};
+        pagination.total = res.total;
+
+        this.setState({
+          numbers: numbers,
+          loading: false,
+          pagination: pagination
+        });
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return(
+      <main className="App">
+        <section className="App-header">
+          <h1>Numbers</h1>
+          <div className="auto" />
+          <span>AVOXI Take-Home</span>
+        </section>
+        <section className="App-card">
+          <Card
+            className="App-card__inner"
+            title="Available Numbers"
+            bordered={true}
+          >
+            <Table
+              dataSource={this.state.numbers}
+              columns={columns}
+              rowKey="id"
+              pagination={this.state.pagination}
+              loading={this.state.loading}
+              onChange={this.onTableChange}
+            />
+          </Card>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default App
--- server.js
+++ server.js
@@ -19,7 +19,101 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/api/numbers", (req, res) => {
-  db.all(`SELECT * FROM Numbers`, (err, rows) => {
+  const page = parseInt(req.query.page) ? parseInt(req.query.page) : 0;
+  const size = parseInt(req.query.size) ? parseInt(req.query.size) : 10;
+  const start = page ? size * (page - 1)
+                     : 0;
+
+  /**
+   * WHERE sub-query was based on:
+   * https://gist.github.com/ssokolow/262503
+   * Scales better by filtering off row id rather than than ordering by
+   * with an offset.
+   */
+  let sql = `
+    SELECT Numbers.id,
+           Numbers.number,
+           Numbers.type,
+           NumberConfiguration.answering_mode,
+           Users.first_name,
+           Users.last_name
+    FROM Numbers 
+      LEFT OUTER JOIN NumberConfiguration
+                   ON Numbers.id=NumberConfiguration.number_id
+      LEFT OUTER JOIN UserNumbers
+                   ON Numbers.id=UserNumbers.number_id
+      LEFT OUTER JOIN Users
+                   ON UserNumbers.user_id=Users.id
+  `
+  if(page) {
+    sql += `
+      WHERE Numbers.oid NOT IN ( SELECT Numbers.oid FROM Numbers
+                                 ORDER BY Numbers.id ASC LIMIT ` +  start + `)
+      `
+  }
+
+  sql += `ORDER BY Numbers.id ASC`;
+
+  if(page) {
+    sql += " LIMIT " + size;
+  }
+
+  console.log(sql);
+
+  db.all(sql, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return res.json({
+        error: true,
+        message: err.message
+      });
+    }
+
+    let sql = `
+      SELECT count(*) from Numbers;
+    `
+    db.get(sql, (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return res.json({
+          error: true,
+          message: err.message
+        });
+      }
+
+      const count = row['count(*)'];
+      const pages = Math.ceil(count / size);
+
+      return res.json({
+        error: false,
+        message: rows,
+        pages: pages,
+        total: count
+      });
+    });
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  db.all(`SELECT * FROM Users`, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    return res.json(rows);
+  });
+});
+
+app.get("/api/usernumbers", (req, res) => {
+  db.all(`SELECT * FROM UserNumbers`, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    return res.json(rows);
+  });
+});
+
+app.get("/api/numberconfiguration", (req, res) => {
+  db.all(`SELECT * FROM NumberConfiguration`, (err, rows) => {
     if (err) {
       console.error(err.message);
     }
